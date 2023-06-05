4b86d990891f6415a0997c03372c216523f33114
MeriamEsprit <meriem.hjiri@esprit.tn>
2023-06-06 00:18:49 +0100

    fonct/metier offer

--- routes/service.js
+++ routes/service.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.post("/add",validate, async (req, res, next) => {
   try {
-    const { name, description, image, phone, email, location } = req.body;
+    const { name, description, image, phone, email, location,date } = req.body;
 
     const checkIfOfferExist = await serviceModel.findOne({ name });
     if (checkIfOfferExist) {
@@ -21,6 +21,7 @@ router.post("/add",validate, async (req, res, next) => {
       image: image,
       phone: phone,
       email: email,
+      date:date,
     });
 
     service.save();
@@ -29,6 +30,7 @@ router.post("/add",validate, async (req, res, next) => {
     res.json(error.message);
   }
 });
+
 router.get("/get/:id", async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -79,6 +81,60 @@ router.post("/search", async (req, res, next) => {
     } else {
       services = await serviceModel.find({ name:{$regex:search} });
     }
+    res.json({ services });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+router.post("/searchFilters", async (req, res, next) => {
+  try {
+    const { name, description, location,date } = req.body;
+    let services = [];
+    if (!services) {
+      services = await serviceModel.find();
+    } else {
+      const query = {};
+      if (name) {
+        query.name = { $regex: name, $options: "i" };
+      }
+      if (description) {
+        query.description = { $regex: description, $options: "i" };
+      }
+      if (location) {
+        query.location = { $regex: location, $options: "i" };
+      }
+      if (date) {
+        const parsedDate = new Date(date);
+        query.date = { $eq: parsedDate };  
+      }
+      services = await serviceModel.find(query).sort({ createdAt: -1 });
+    }
+    res.json({ services });
+  } catch (error) {
+    res.json(error.message);
+  }
+});
+
+router.post("/sort", async (req, res, next) => {
+  try {
+    const { field, order } = req.body;
+
+    let sortValue;
+    if (order === "asc") {
+      sortValue = 1; 
+    } else {
+      sortValue = -1; 
+    }
+    console.log(field,order);
+    const sortOptions = {};
+    sortOptions[field] = sortValue;
+
+    const services = await serviceModel
+    .find()
+    .collation({ caseLevel:true,locale:"en_US" })
+    .sort(sortOptions).limit(5);    
+    
     res.json({ services });
   } catch (error) {
     res.json(error.message);
